3fe88d40e173d63604ae9da3ae31811c9db793cb
td <td@gmail.com>
2024-04-23 11:33:14 +0200

    update

--- 3kep_/galery.js
+++ 3kep_/galery.js
@@ -1,8 +1,11 @@
-_tomb = ['alma', 'körte', 'kutya', 'macska'];
+_tomb = ['alma', 'körte', 'kutya', 'macska']
 
-console.log('Hello');
-const _div = document.createElement('div');
-_div.innerHTML += 'Hello World';
-document.body.appendChild(_div);
+console.log('Hello')
+const _div = document.createElement('div')
+_div.innerHTML += 'Hello World'
+document.body.appendChild(_div)
 
 //console.log(_tomb);
+
+for (t of _tomb) {
+}

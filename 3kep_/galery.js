2b033f678c8eae3e1de27261a155020094f783c6
6gDav <118929714+6gDav@users.noreply.github.com>
2024-04-23 08:36:41 +0200

    Add files via upload

---
+++ 3kep_/galery.js
@@ -0,0 +1,8 @@
+_tomb = ['alma', 'körte', 'kutya', 'macska'];
+
+console.log('Hello');
+const _div = document.createElement('div');
+_div.innerHTML += 'Hello World';
+document.body.appendChild(_div);
+
+//console.log(_tomb);

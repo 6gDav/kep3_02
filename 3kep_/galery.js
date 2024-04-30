906da60fb6b88077d3a2398ad8fab8912a73133e
td <td@gmail.com>
2024-04-30 08:35:50 +0200

    update 04_30

--- 3kep_/galery.js
+++ 3kep_/galery.js
@@ -3,17 +3,19 @@ _tomb = ['alma', 'körte', 'kutya', 'macska']
 console.log('Hello')
 const _div = document.createElement('div')
 _div.innerHTML += 'Hello World'
-document.body.appendChild(_div)
+//document.body.appendChild(_div)
 
 //console.log(_tomb);
 
 for (t of _tomb) {
   const _div = document.createElement('div')
   _div.innerHTML += t
-  document.body.appendChild(_div)
+ // document.body.appendChild(_div)
 }
 
 let kep_lista = ['cica_kep.jpg', 'delfin_kep.webp', 'kutya_kep.png'];
+
+const _targetImg = document.querySelector('#targetImg');
 
 for(el of kep_lista)
 {
@@ -23,7 +25,8 @@ for(el of kep_lista)
   console.log(kep_elem)
   document.body.appendChild(kep_elem)
 
-  kep_elem.addEventListener('click', ()=>{
-    window.open('cica_kep.html')
+  kep_elem.addEventListener('click', function(){
+    console.log(this.src);
+    _targetImg.src = this.src;
   });
 }
